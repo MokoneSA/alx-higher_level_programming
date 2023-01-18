fc2bf864108e39e5b9e0ddf4f639146c25b0abe4
MokoneSA <simonlephotojr@gmail.com>
2023-01-18 13:56:01 +0000

    0. If it's not tested it doesn't work

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a base model class."""
+
+import json
+import csv
+import turtle
+
+class Base:
+    """Base model.
+
+    This Represent the "base" for alll other classes in the project 0x0C*.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
+
+    __nb_objects = 0
+
+    def __inti__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
