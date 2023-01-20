03def9d56bf3ec1bcda4ef57936bb2dcb539193e
MokoneSA <simonlephotojr@gmail.com>
2023-01-20 13:33:09 +0000

    3. Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Define a rectangle class"""
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -26,3 +26,59 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """set/get the width of the Rectangle"""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self._width = value
+
+    @property
+    def height(self):
+        """set/get the height of the Rectangle"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self._height = value
+
+    @property
+    def x(self):
+        """set/get the x of the Rectangle"""
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+            self.__x = value
+
+    @property
+    def y(self):
+        """Set/get the y coordinate of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.width * self.height
