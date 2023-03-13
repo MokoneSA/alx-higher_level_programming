4c71fa5ce1fed1e5393485c702670b4d21f9fbba
MokoneSA <simonlephotojr@gmail.com>
2023-03-13 13:33:31 +0000

    1. 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myVar0 = 'C is fun';
+const myVar1 = 'Python is fun';
+const myVar2 = 'JavaScript is amazin';
+console.log(myVar0);
+console.log(myVar1);
+console.log(myVar2);
